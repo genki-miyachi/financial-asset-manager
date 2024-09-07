169b071dbb2192de13ea1bd925ee46a93743f096
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-09-07 17:36:27 +0900

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello from GraphQL!'
+  }
+};
+
+async function startApolloServer() {
+  const app = express();
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+}
+
+startApolloServer();
